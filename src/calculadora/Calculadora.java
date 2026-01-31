8de148ac2ace58340fa4321527408025b3f1290c
Pc39 <Pc39@CPU39>
2026-01-31 10:03:34 -0600

    Principal

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Bienvenido a la calculadora de la mesa 13");
+        System.out.println("Ingresa un numero");
+        int a= sc.nextInt();
+        System.out.println("Ingresa otro numero");
+        int b= sc.nextInt();
+        
+        //Suma suma = new Suma();
+        //Resta resta = new Resta();
+        
+        //System.out.println("La suma de "+a+" + "+b+" es: "+suma.calcular(a,b));
+        //System.out.println("La resta de "+a+" + "+b+" es: "+suma.calcular(a,b));
+        
+    }
+    
+}
